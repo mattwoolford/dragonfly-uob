3f85f0ea1fdb706851a0240720a2ae69ff6b07d2
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-02-15 19:50:04 +0000

    DF-39 - Create `Geolocation` mission module - Create a test file for the `Geolocation` mission module - Create a run configuration for the `Geolocation` mission module test file

---
+++ server/mission_modules/Geolocation/TestGeolocation.py
@@ -0,0 +1,25 @@
+import unittest
+
+from server.mission_modules.Geolocation.Geolocation import Geolocation
+
+
+class TestSearch(unittest.TestCase):
+
+    """
+    Create your tests here
+
+    For example:
+    """
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+    """
+    Test the `start` method
+    """
+    def test_start(self):
+        self.assertTrue(callable(Geolocation.start)) # Replace this example
+
+
+if __name__ == '__main__':
+    unittest.main()
