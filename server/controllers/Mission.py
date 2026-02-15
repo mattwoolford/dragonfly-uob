b5bf7b7ca2121a3d73b322ebf65422e227ececdb
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-02-15 20:17:00 +0000

    DF-40 - Refactor from `MissionController` to `Mission` - Untie `Mission` from `MissionModule` interface

---
+++ server/controllers/Mission.py
@@ -0,0 +1,16 @@
+class Mission:
+
+    """
+
+    This mission controller acts as a state machine that guides the aircraft through a search and rescue mission.
+
+    """
+
+    def _init__(self, options):
+        """
+        Start the mission.
+
+        If you need information to start with, then these can be provided in the
+        `options` parameter.
+        """
+        pass
---
+++ server/controllers/TestMission.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestMission(unittest.TestCase):
+
+    """
+    Create your tests here
+
+    For example:
+    """
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
