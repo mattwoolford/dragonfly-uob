6985eaf0ff8990a4a80cebfa9beb5500b802627e
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-02-15 20:18:11 +0000

    DF-40 - Create `Aircraft` control module - Create a test file for the `Aircraft` control module - Create a run configuration for the `Aircraft` control module test file

---
+++ server/controllers/Aircraft.py
@@ -0,0 +1,25 @@
+class Aircraft:
+
+    """
+
+    This controller acts as a controller sending flight commands to the aircraft.
+
+    """
+
+    def arm(self, options):
+        """
+        Arm the aircraft.
+        """
+        pass
+
+    def takeoff(self, altitude):
+        """
+        Take-off the aircraft to a specified altitude.
+        """
+        pass
+
+    def land(self, options):
+        """
+        Land the aircraft.
+        """
+        pass
