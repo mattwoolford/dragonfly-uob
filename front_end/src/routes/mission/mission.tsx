283308882558ec65882e464cb12e8f90278a0e56
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-03-09 21:49:26 +0000

    DF-54 - Create entry animation

--- front_end/src/routes/mission/mission.tsx
+++ front_end/src/routes/mission/mission.tsx
@@ -2,11 +2,28 @@ import './mission.css';
 import LoadingIcon from "@components/LoadingIcon/LoadingIcon.tsx";
 
 
+export async function clientLoader() {
+  // TODO: Do setup here
+  await Promise.all([
+                      new Promise(resolve => setTimeout(resolve, 10000))
+                    ]);
+  return null;
+}
+
+
+clientLoader.hydrate = true;
+
+
+export function HydrateFallback() {
+  return <LoadingIcon />
+}
+
+
 export default function Mission() {
 
   return (
     <>
-      <LoadingIcon />
+      <p>Loaded</p>
     </>
   )
 }
