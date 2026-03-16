0bd0852575df049919fde16db811d0eb570bc618
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-03-16 23:25:52 +0000

    DF-54 DF-87 - Create a map with an icon and implement into the mission view

--- front_end/src/routes/mission/mission.tsx
+++ front_end/src/routes/mission/mission.tsx
@@ -1,6 +1,13 @@
 import './mission.css';
-import KeyboardEffect from "@utils/components/KeyboardEffect/KeyboardEffect.tsx";
-import LoadingIcon    from "@components/LoadingIcon/LoadingIcon.tsx";
+import KeyboardEffect   from "@utils/components/KeyboardEffect/KeyboardEffect.tsx";
+import LoadingIcon      from "@components/LoadingIcon/LoadingIcon.tsx";
+import ContentContainer from "@components/ContentContainer/ContentContainer.tsx";
+import {
+    LayoutGroup,
+    motion
+}                       from "motion/react";
+import Console          from "@components/Console/Console.tsx";
+import Map              from "@components/Map/Map.client.tsx";
 
 
 export async function clientLoader() {
@@ -22,11 +29,22 @@ export function HydrateFallback() {
 
 export default function Mission() {
 
-  return (
-    <>
-      <section className={"px-4"}>
-        <p className={"font-bold text-2xl"}><KeyboardEffect showEndCursor={false}>DragonFly Mission Control</KeyboardEffect></p>
-      </section>
-    </>
-  )
+    const DEFAULT_LOCATION_PIN = [51.423967391658536, -2.67169820644399] satisfies [number, number];
+
+    return (
+        <>
+            <section className={"px-4"}>
+                <p className={"font-bold text-2xl"}><KeyboardEffect showEndCursor={false}>DragonFly Mission Control</KeyboardEffect></p>
+            </section>
+            <motion.section className={"flex flex-row justify-between items-center gap-4 px-4 my-2 py-2 w-full min-h-100"}>
+                <LayoutGroup>
+                    <ContentContainer>
+                        <Map pin={ DEFAULT_LOCATION_PIN } onMapClick={(lat, lng) => console.log(`Clicked: ${lat}, ${lng}`)} />
+                    </ContentContainer>
+                    <ContentContainer />
+                    <Console />
+                </LayoutGroup>
+            </motion.section>
+        </>
+    )
 }
