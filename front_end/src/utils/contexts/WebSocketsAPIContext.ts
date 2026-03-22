6d3eff3925287d4d3bad0e72e7cf4dc2f094c39d
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-03-22 14:05:23 +0000

    DF-54 DF-56 - Remove websockets API - Reimplement websockets with a test message

--- front_end/src/utils/contexts/WebSocketsAPIContext.ts
+++
@@ -1,6 +0,0 @@
-import {createContext} from "react";
-import type {Socket} from "socket.io-client";
-
-const WebSocketAPIContext = createContext<Socket | null>(null);
-
-export default WebSocketAPIContext;
