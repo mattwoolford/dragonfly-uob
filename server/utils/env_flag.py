7ad21a9472eb3a3158d4f57327f4c28158dbc635
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-03-22 14:17:21 +0000

    DF-55 - Remove HTTP template route - Refactor `env_flag` utility function to **/utils**

---
+++ server/utils/env_flag.py
@@ -0,0 +1,8 @@
+import os
+
+
+def env_flag(name: str, default: bool = False) -> bool:
+    value = os.getenv(name)
+    if value is None:
+        return default
+    return value.strip().lower() in {"1", "true", "yes", "on"}
