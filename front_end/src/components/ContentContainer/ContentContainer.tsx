7860c344da7e13c374be42861ff4479189c08115
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-03-23 22:39:49 +0000

    DF-89 DF-91 - Add menu to image selection to allow submission of pixels during image interrogation - Add selection step to image interrogation, marking user selection - Improve animations of expanding `ContentContainer` windows - Add `Button` element for standardised, interactive buttons - Add prompt to image interrogation step - Add ability to send null coordinates and cancel image interrogation

--- front_end/src/components/ContentContainer/ContentContainer.tsx
+++ front_end/src/components/ContentContainer/ContentContainer.tsx
@@ -5,14 +5,40 @@ import {
 import clsx from "clsx";
 
 
-export default function ContentContainer({ children, layout: _layout, className, ...props }: HTMLMotionProps<"article">) {
+interface ContentContainerProps extends HTMLMotionProps<"article"> {
+    maximise?: boolean;
+}
 
-    const cx = clsx("ContentContainer", className);
-
+export default function ContentContainer({
+                                             children,
+                                             className,
+                                             maximise = false,
+                                             style,
+                                             ...props
+                                         }: ContentContainerProps) {
     return (
-        <motion.article className={cx} layout {...props}>
+        <motion.article
+            className={clsx("ContentContainer", className)}
+            layout
+            style={{
+                position: maximise ? "fixed" : "relative",
+                inset: maximise ? 0 : undefined,
+                width: maximise ? "100vw" : undefined,
+                height: maximise ? "calc(100vh - (var(--spacing) * 20))" : undefined,
+                marginTop: maximise ? "calc(var(--spacing) * 20)" : undefined,
+                ...style,
+            }}
+            transition={{
+                layout: {
+                    type: "spring",
+                    stiffness: 280,
+                    damping: 30,
+                    duration: 0.5
+                },
+            }}
+            {...props}
+        >
             {children}
         </motion.article>
-    )
-
+    );
 }
--- front_end/src/components/ImageCanvas/ImageCanvas.client.tsx
+++ front_end/src/components/ImageCanvas/ImageCanvas.client.tsx
@@ -1,17 +1,25 @@
 import {
-    Activity,
     type MouseEventHandler,
-    type ReactEventHandler,
+    useCallback,
     useEffect,
     useRef,
     useState
-}             from "react";
-import socket from "../../socket";
-import Logo   from "@assets/logo/png/DragonFly Logo_Emblem.png";
+}                     from "react";
+import {
+    AnimatePresence,
+    motion
+}                     from "motion/react";
+import socket         from "../../socket";
+import Logo           from "@assets/logo/png/DragonFly Logo_Emblem.png";
+import Button         from "@components/Button/Button.tsx";
+import KeyboardEffect from "@utils/components/KeyboardEffect/KeyboardEffect.tsx";
 
 
 interface ImageCanvasProps {
-    onPixelClick?: (x: number, y: number) => void;
+    onCancel?: () => void;
+    onImageLoad?: () => void;
+    onPixelClick?: (u: number, v: number) => void;
+    onPixelSelection?: (u: number, v: number) => void;
     socketEvent?: string;
 }
 
@@ -21,23 +29,86 @@ type ImageSocketPayload = {
     }
 };
 
-export default function ImageCanvas({ socketEvent = "image-inspection" }: ImageCanvasProps) {
+type ImageDimensions = {
+    height: number,
+    width: number
+};
 
+type ImagePixelSelection = {
+    u: number,
+    v: number
+};
+
+export default function ImageCanvas({ socketEvent = "image-inspection", onCancel, onImageLoad, onPixelClick, onPixelSelection }: ImageCanvasProps) {
+
+    // State
+    // Pixel coordinates that have been selected by the user
+    const [selectedPixels, setSelectedPixels] = useState<ImagePixelSelection | null>(null);
+    // Pixel coordinates that have been selected by the user, used independently for marking the location
+    const [selectedMarker, setSelectedMarker] = useState<ImagePixelSelection | null>(null);
+    // Whether a submission of selected pixels has been made, or the operation has been cancelled
+    const [submitted, setSubmitted] = useState<boolean>(false);
+    // The source image data / URI
     const [imageSrc, setImageSrc] = useState<string | null>(null);
-    const imgRef = useRef<HTMLImageElement>(null);
+    // The dimensions of the image
+    const [imageDimensions, setImageDimensions] = useState<ImageDimensions | null>(null);
 
-    const handleClick: MouseEventHandler<HTMLImageElement> = (e) => {
 
-        if (!imgRef.current) return;
+    // Refs
+    // The canvas being drawn to display the image and marker
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    // The image being loaded and respectively drawn onto the canvas
+    const imageRef = useRef<HTMLImageElement | null>(null);
 
-        const rect = imgRef.current.getBoundingClientRect();
-        const scaleX = imgRef.current.naturalWidth / rect.width;
-        const scaleY = imgRef.current.naturalHeight / rect.height;
+
+    // Callbacks
+    // Handle user clicking the canvas to select pixels
+    const handlePixelSelection: MouseEventHandler<HTMLCanvasElement> = (e) => {
+
+        if (!canvasRef.current || !imageDimensions) return;
+
+        const rect = canvasRef.current.getBoundingClientRect();
+        const scaleX = imageDimensions.width / rect.width;
+        const scaleY = imageDimensions.height / rect.height;
 
         const u = Math.round((e.clientX - rect.left) * scaleX);
         const v = Math.round((e.clientY - rect.top) * scaleY);
 
-        console.log("Image click", u, v);
+        onPixelClick?.(u, v);
+        setSelectedPixels({u, v});
+        setSelectedMarker({u, v});
+
+    };
+
+    // Handle user cancelling the pixel selection operation
+    const handleCancelSelection = useCallback(() => {
+
+        onCancel?.();
+
+        // Submit null coordinates to convey that the operation was cancelled
+        socket.emit("pixel-coordinates-selected", {
+            data: {
+                u: null,
+                v: null
+            }
+        });
+
+        setSelectedPixels(null);
+        setSelectedMarker(null);
+        setImageDimensions(null);
+        setImageSrc(null);
+        setSubmitted(true);
+
+    }, [onCancel]);
+
+    // Handle submitting a pixel selection
+    const handleSubmitPixels = useCallback(() => {
+
+        if(!selectedPixels) return;
+
+        const {u, v} = selectedPixels;
+
+        onPixelSelection?.(u, v);
 
         socket.emit("pixel-coordinates-selected", {
             data: {
@@ -46,19 +117,28 @@ export default function ImageCanvas({ socketEvent = "image-inspection" }: ImageC
             }
         });
 
-    };
+        setSelectedPixels(null);
+        setSelectedMarker(null);
+        setImageDimensions(null);
+        setImageSrc(null);
+        setSubmitted(true);
 
-    const handleError: ReactEventHandler<HTMLImageElement> = (e) => {
-        e.currentTarget.setAttribute("src", Logo);
-    };
+    }, [onPixelSelection, selectedPixels]);
 
+
+    // Effects
+    // Initialise socket, or if the socket event to listen for changes, update the socket listener
     useEffect(() => {
 
         socket.on(socketEvent, (payload: ImageSocketPayload) => {
             const imagePayload = payload.data.image;
 
             const blob = new Blob([imagePayload], { type: "image/jpeg" });
+            setSelectedPixels(null);
+            setSelectedMarker(null);
+            setImageDimensions(null);
             setImageSrc(URL.createObjectURL(blob));
+            setSubmitted(false);
         });
 
         return () => {
@@ -66,23 +146,138 @@ export default function ImageCanvas({ socketEvent = "image-inspection" }: ImageC
         };
     }, [socketEvent]);
 
+    // Load an image to apply image data to the canvas when the image source changes
+    useEffect(() => {
 
+        if (!imageSrc) {
+            imageRef.current = null;
+            return;
+        }
+
+        const image = new Image();
+
+        image.onload = () => {
+            imageRef.current = image;
+            setImageDimensions({
+                width: image.naturalWidth,
+                height: image.naturalHeight
+            });
+            onImageLoad?.();
+        };
+
+        image.src = imageSrc;
+
+        return () => {
+            imageRef.current = null;
+            image.onload = null;
+            URL.revokeObjectURL(imageSrc);
+        };
+    }, [imageSrc, onImageLoad]);
+
+    // Draw the image and selection marker if applicable
+    useEffect(() => {
+
+        if (!canvasRef.current || !imageRef.current || !imageDimensions) return;
+
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+
+        if (!context) return;
+
+        // Draw the image
+        canvas.width = imageDimensions.width;
+        canvas.height = imageDimensions.height;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(imageRef.current, 0, 0, canvas.width, canvas.height);
+
+        // Draw the selection marker
+        if (!selectedMarker) return;
+
+        const MARKER_RADIUS = 40;
+
+        context.beginPath();
+        context.arc(selectedMarker.u, selectedMarker.v, MARKER_RADIUS, 0, Math.PI * 2);
+        context.fillStyle = "transparent";
+        context.fill();
+        context.lineWidth = 4;
+        context.strokeStyle = "rgb(198, 44, 44)";
+        context.stroke();
+    }, [imageDimensions, selectedMarker]);
+
+
+    // Render
     return (
         <div className={"ImageCanvas"}>
-            <div className={"ImageCanvas__background"}>
-                <img className={"size-40"} src={Logo} alt={"DragonFly Logo"}/>
-                <p>Mission not started</p>
-            </div>
-            <Activity mode={ imageSrc ? "visible" : "hidden"}>
-                <img
-                    ref={imgRef}
-                    src={imageSrc ?? ""}
-                    onClick={handleClick}
-                    className={"ImageCanvas__image"}
-                    alt={ "Air Imagery" }
-                    onError={handleError}
-                />
-            </Activity>
+            <AnimatePresence>
+                {
+                    !imageSrc && (
+                                  <motion.div className={"ImageCanvas__background"}
+                                              initial={{
+                                                  opacity: 0
+                                              }}
+                                              animate={{
+                                                  opacity: 1,
+                                                  transition: {
+                                                      delay: 0.5,
+                                                      duration: 0.5
+                                                  }
+                                              }}
+                                              exit={{
+                                                  opacity: 0
+                                              }}
+                                              transition={{
+                                                  delay: 0,
+                                                  duration: 0.5
+                                              }}
+                                  >
+                                      <img className={"size-40"} src={Logo} alt={"DragonFly Logo"}/>
+                                      <p>Mission not started</p>
+                                  </motion.div>
+                              )
+                }
+            </AnimatePresence>
+            <AnimatePresence>
+                {
+                    !!imageSrc && (
+                                    <motion.div
+                                        className={"ImageCanvas__image-container"}
+                                        initial={{
+                                            opacity: 0
+                                        }}
+                                        animate={{
+                                            opacity: 1
+                                        }}
+                                        exit={{
+                                            opacity: 0,
+                                            transition: {
+                                                delay: 0
+                                            }
+                                        }}
+                                        transition={{
+                                            delay: 0.5,
+                                            duration: 0.2
+                                        }}>
+                                        <canvas
+                                            ref={canvasRef}
+                                            onClick={handlePixelSelection}
+                                            className={"ImageCanvas__image"}
+                                            width={imageDimensions?.width}
+                                            height={imageDimensions?.height}
+                                        />
+                                        <form className={"ImageCanvas__form"} onSubmit={(e) => e.preventDefault()}>
+                                            <Button onClick={handleCancelSelection}
+                                                    disabled={submitted}
+                                            >No Subject Found</Button>
+                                            <KeyboardEffect showStartCursor={false}>Task: Select the target subject in the image</KeyboardEffect>
+                                            <Button theme={"var(--color-active)"}
+                                                    disabled={!selectedPixels || submitted}
+                                                    onClick={handleSubmitPixels}>Send Subject Location</Button>
+                                        </form>
+                                    </motion.div>
+                               )
+                }
+            </AnimatePresence>
         </div>
     );
 }
