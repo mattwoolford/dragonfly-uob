649b1d9f691588b56e74ef7a28cd623931f4547b
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2026-03-26 19:27:24 +0000

    DF-95 - Add ability to fetch and update the current mission status, including displaying an image currently being assessed - Remove debug console logs

--- server/controllers/Mission.py
+++ server/controllers/Mission.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+import time
 from pathlib import Path
 from typing import Any, Callable
 
@@ -10,12 +12,17 @@ class Mission:
     """
 
     def __init__(self, socketio_instance=None):
+        self._assessment_image = None
         self.socketio = socketio_instance  # SocketIO instance
+        self.set_status("Mission not started") # Initialise the mission status
         self.steps_queue = []  # Steps to be executed (list of methods)
         self.target_coordinates = None  # Coordinates of SAR subject
 
     def add_step(self, next_step: Callable):
         self.steps_queue.append(next_step)
+
+    def get_image_for_assessment(self):
+        return self._assessment_image
 
     def _initiate_sockets(self, options: dict[str, Any] | None = None):
         socketio_instance = options.get("socketio") or self.socketio
@@ -29,6 +36,7 @@ class Mission:
     def _send_image_for_assessment(self, file_path):
         with open(file_path, "rb") as f:
             image_bytes = f.read()
+            self._assessment_image = image_bytes
             self.socketio.emit("image-inspection", {
                 "data": {
                     "image": image_bytes
@@ -37,6 +45,7 @@ class Mission:
 
     def set_target_coordinates(self, coordinates: tuple[float, float]):
         self.target_coordinates = coordinates
+        self.set_status("Target found")
         # TODO: Set next step to be geolocation from image
         print(f"Target coordinates set to {coordinates}")
 
@@ -47,9 +56,22 @@ class Mission:
     # TODO: Replace with actual search module
     def _search(self):
         # TODO: Replace with image from search
+        self.set_status("Navigating to the search area...")
+        time.sleep(5)
+        self.set_status("Image capture in progress...")
+        time.sleep(5)
         BASE_DIR = Path(__file__).resolve().parent
         file_path = f"{BASE_DIR}/../assets/test-image.png"
         self._send_image_for_assessment(file_path)
+
+    def set_status(self, status: str):
+        self.status = status
+        self.socketio.emit("mission-status-change", {
+            "data": {
+                "missionStatus": status
+            }
+        })
+
 
     def start(self, options: dict[str, Any] | None = None):
         """
@@ -61,6 +83,10 @@ class Mission:
 
         options = options or { }
         socketio = self._initiate_sockets(options)
+
+        # Update mission status to reflect start
+        self.set_status("Mission started")
+        time.sleep(2)
 
         # TODO: Enter search loop
         # while not self.target_coordinates:
